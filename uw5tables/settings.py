d4a73f35dd568358364c4609119477f45a73484c
Ubaidhq <u.hoque@warwick.ac.uk>
2020-09-16 19:36:45 +0100

    Add dependecies
    Add dependencies needed for django.

--- uw5tables/settings.py
+++ uw5tables/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'oee!9gp6g7q$c3qwq8aa$b8gi(p#h$fkk=g1i0rr0fjmbr@=zw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.71']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
